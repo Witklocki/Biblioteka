ceab5f4a078e4582a874b734ebe263ecca3e35b4
P.Witkowski <paweksx@poczta.onet.pl>
2020-07-15 12:00:34 +0200

    Added navigation folder

--- angularCardIndex/src/app/app.module.ts
+++ angularCardIndex/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {ServernaukaService} from './servernauka.service';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './views/general/navigation/navigation.component';
 
 
 
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
     routingComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
