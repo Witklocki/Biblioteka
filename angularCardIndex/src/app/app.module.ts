4de50ebdb964de01d2b945b4104a9b6639a87ec2
Patryk Jugowiec <p.jugowiec@wasko.pl>
2020-07-15 13:22:44 +0200

    Codereview  #2

--- angularCardIndex/src/app/app.module.ts
+++ angularCardIndex/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule, routingComponent} from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ServernaukaService} from './servernauka.service';
-import {HttpClientModule} from '@angular/common/http';
+import { ServernaukaService } from './servernauka.service';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './views/general/navigation/navigation.component';
 
 
 
-
+/*
+* Zrobie Ci jeszcze dodatkowo code review jak ogarniesz tutaj:
+- Czyść kod zanim spushujesz - to że to nie jest kod produkcyjny to nie znaczy że nie ma mieć standardów
+- Żadnych console.logów nie powinno być na masterze
+- Poczytaj co to jest metoda a co to jest model
+- Poświęć chwile i wejdz https://github.com/pjugowiec/cardIndexApp/tree/master/angularCardIndex/src, przejrzyj strukture, zobacz co gdzie jest umiejscowione
+*/
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,8 +30,8 @@ import { NavigationComponent } from './views/general/navigation/navigation.compo
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    
-    
+
+
   ],
   providers: [ServernaukaService],
   bootstrap: [AppComponent]
