2d2017eef68a83371178b1d45c1f0a5387450619
Patryk Jugowiec <p.jugowiec@wasko.pl>
2020-07-16 07:18:41 +0200

    Codereview #3.1

--- angularCardIndex/src/app/app.module.ts
+++ angularCardIndex/src/app/app.module.ts
@@ -9,15 +9,6 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './views/general/navigation/navigation.component';
 
-
-
-/*
-* Zrobie Ci jeszcze dodatkowo code review jak ogarniesz tutaj:
-- Czyść kod zanim spushujesz - to że to nie jest kod produkcyjny to nie znaczy że nie ma mieć standardów
-- Żadnych console.logów nie powinno być na masterze
-- Poczytaj co to jest metoda a co to jest model
-- Poświęć chwile i wejdz https://github.com/pjugowiec/cardIndexApp/tree/master/angularCardIndex/src, przejrzyj strukture, zobacz co gdzie jest umiejscowione
-*/
 @NgModule({
   declarations: [
     AppComponent,
